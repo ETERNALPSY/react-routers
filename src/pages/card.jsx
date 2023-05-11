685a368f2f88e9c0779c4bde6fae650be023e271
eternal <tr.dev.manueldominguez@gmail.com>
2023-05-11 07:01:53 -0600

    feat: add details card

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from "react-router-dom"
 import GetCharacters from "../services/api"
 import { useEffect, useState } from "react"
+import "./card.scss"
 
 const CharacterCard = () => {
    const params = useParams()
@@ -17,10 +18,12 @@ const CharacterCard = () => {
    return (
       <>
          <div className="card col-12 col-md- 8 mx-auto" data-aos="zoom-in" data-aos-duration="2000">
+            <h4 classNme="card-title fs-1 ">{character.name}</h4>
             <img src={character.image} classNme="card-img" />
             <div classNme="card-body">
-               <h5 classNme="card-title fs-1 fw-bold">{character.name}</h5>
+
                <p classNme="card-text fs-6">{character.species}</p>
+               <p classNme="card-text fs-6">{character.gender}</p>
             </div>
             <Link className=' btn btn-secondary fs-5 col-4 m-auto mb-2' to="/">
                Volver
